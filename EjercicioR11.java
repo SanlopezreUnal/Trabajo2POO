9a255250ac2be8bfb0405a9723c0bdd5076933f9
SanlopezreUnal <sanlopezre@unal.edu.co>
2024-12-08 17:16:01 -0500

    Create EjercicioR11.java

---
+++ EjercicioR11.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class EjercicioRR11 {
+
+    public static void main(String[] args) {
+        double n1,n2,n3,mayor;
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        
+        System.out.println("Escriba el valor de n1");
+        n1 = entrada.nextDouble();
+        
+        System.out.println("Escriba el valor de n2");
+        n2 = entrada.nextDouble();
+        
+        System.out.println("Escriba el valor de n3");
+        n3 = entrada.nextDouble();
+        
+        if ((n1>n2) && (n1>n3)) {
+            mayor = n1;
+        }
+        else if (n2>n3) {
+            mayor = n2;
+        }
+        else {
+            mayor = n3;
+           
+        }
+        
+        System.out.println("El mayor entre: "+ n1 + ", "+ n2 + " y "+ n3 + " es "+ mayor);
+        
+    }
+}
